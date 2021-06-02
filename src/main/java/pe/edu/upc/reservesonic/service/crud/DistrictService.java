dc37205b8692e62453fae26c9083918be9dd9322
JossMass <jmascaro2000@gmail.com>
2021-06-02 00:03:33 -0500

    add Service Crud

---
+++ src/main/java/pe/edu/upc/reservesonic/service/crud/DistrictService.java
@@ -0,0 +1,7 @@
+package pe.edu.upc.reservesonic.service.crud;
+
+import pe.edu.upc.reservesonic.model.entity.District;
+
+public interface DistrictService extends CrudService<District, Integer>{
+
+}
---
+++ src/main/java/pe/edu/upc/reservesonic/service/crud/UserService.java
@@ -0,0 +1,5 @@
+package pe.edu.upc.reservesonic.service.crud;
+
+public interface UserService {
+
+}
