08ec981534f06f75c96c6559a961f9ec28aabdca
JossMass <jmascaro2000@gmail.com>
2021-06-02 23:48:46 -0500

    add findByIds RoomController

--- src/main/java/pe/edu/upc/reservesonic/controller/RoomController.java
+++ src/main/java/pe/edu/upc/reservesonic/controller/RoomController.java
@@ -1,16 +1,19 @@
 package pe.edu.upc.reservesonic.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pe.edu.upc.reservesonic.model.entity.Room;
+import pe.edu.upc.reservesonic.model.entity.Studio;
 import pe.edu.upc.reservesonic.service.crud.RoomService;
 
 @Controller
@@ -18,7 +21,7 @@ import pe.edu.upc.reservesonic.service.crud.RoomService;
 public class RoomController {
 	@Autowired
 	private RoomService roomService;
-	
+
 	@GetMapping
 	public String list(Model model) {
 		try {
@@ -28,23 +31,23 @@ public class RoomController {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
-		
+
 		return "rooms/listRooms";
 	}
-	
+
 	@PostMapping("save")
 	public String saveEdit(Model model, @ModelAttribute("roomEdit") Room room) {
 		try {
 			Room roomReturn = roomService.update(room);
 			model.addAttribute("room", roomReturn);
 			return "rooms/view";
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
 		return "redirect:/rooms";
 	}
-	
+
 	@GetMapping("new")
 	public String newRoom(Model model) {
 		try {
@@ -55,10 +58,10 @@ public class RoomController {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
-		
+
 		return "redirect:/rooms";
 	}
-	
+
 	@PostMapping("savenew")
 	public String saveNew(Model model, @ModelAttribute("roomNew") Room room) {
 		try {
@@ -69,7 +72,38 @@ public class RoomController {
 			e.printStackTrace();
 			System.err.println(e.getMessage());
 		}
-		
+
+		return "redirect:/rooms";
+	}
+
+	// Id's
+	@GetMapping("{id}") // GET: /rooms/{id}
+	public String findById(Model model, @PathVariable("id") Integer id) {
+		try {
+			Optional<Room> optional = roomService.findById(id);
+			if (optional.isPresent()) {
+				model.addAttribute("room", optional.get());
+				return "rooms/view";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "redirect:/rooms";
+	}
+
+	@GetMapping("{id}/edit") // GET: /rooms/{id}/edit
+	public String findById2(Model model, @PathVariable("id") Integer id) {
+		try {
+			Optional<Room> optional = roomService.findById(id);
+			if (optional.isPresent()) {
+				model.addAttribute("roomEdit", optional.get());
+				return "rooms/edit";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
 		return "redirect:/rooms";
 	}
 }
