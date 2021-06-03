b7345ab1d3e3c92d0861593220ff89760b18d5ea
JossMass <jmascaro2000@gmail.com>
2021-06-02 23:04:06 -0500

    add Studio Controller

--- src/main/java/pe/edu/upc/reservesonic/controller/StudioController.java
+++ src/main/java/pe/edu/upc/reservesonic/controller/StudioController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pe.edu.upc.reservesonic.model.entity.Studio;
@@ -28,4 +30,47 @@ public class StudioController {
         }
         return "studios/listStudios";
     }
+    
+    @PostMapping("save")
+    public String saveEdit(Model model, @ModelAttribute("studioEdit") Studio studio) {
+		try {
+			Studio studioReturn = studioService.update(studio);
+			model.addAttribute("studio", studioReturn);
+			return "studios/view";
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "redirect:/studios";
+	}
+    
+    @GetMapping("new")
+	public String newStudio(Model model) {
+		try {
+			Studio studio = new Studio();
+			model.addAttribute("roomNew", studio);
+			return "studios/new";
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		
+		return "redirect:/studios";
+	}
+    
+	@PostMapping("savenew")
+	public String saveNew(Model model, @ModelAttribute("studioNew") Studio studio) {
+		try {
+			Studio studioReturn = studioService.create(studio);
+			model.addAttribute("studio", studioReturn);
+			return "studios/view";
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		
+		return "redirect:/studios";
+	}
+    
+    
 }
